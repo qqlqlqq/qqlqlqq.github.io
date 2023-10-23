55b4329485109d673c0e9b545f7fdfcdace24c9a
qqlqlqq <qqvce123@proton.me>
2023-10-23 14:24:49 +0800

    Site updated: 2023-10-23 14:24:49

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,4 @@
+// 动态心跳
 $(document).ready(function(e) {
     $('.copyright').html('©2021 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By 竹山一叶');
 })
@@ -10,7 +11,7 @@ $(document).ready(function(e) {
 function show_date_time() {
 $('.framework-info').html('本站已运行<span id="span_dt_dt" style="color: #fff;"></span>');
   window.setTimeout("show_date_time()", 1000);
-  BirthDay=new Date("10/21/2023 0:0:0");
+  BirthDay=new Date("1/20/2021 0:0:0");
   today=new Date();
   timeold=(today.getTime()-BirthDay.getTime());
   sectimeold=timeold/1000
